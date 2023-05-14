23ff9de9ad45a089dc750452df3e10063ca77d31
Dimitris Vassiliou <dhmhtrhs.vassiliou@gmail.com>
2023-05-14 14:03:28 +0300

    update ast

--- interpreter/Ast.ml
+++ interpreter/Ast.ml
@@ -124,16 +124,89 @@ let newFuncCall (a, b) = {
 let rec print_funcDef funcDef = 
   Printf.printf("FuncDef("); print_header funcDef.header; List.iter print_localDef funcDef.local_def_list; List.iter print_stmt funcDef.block; Printf.printf(")");
 
+
 let rec print_header header = 
   Printf.printf("Header(fun("); Printf.print("%s") id; Printf.printf ("(") List.iter print_fparDef header.fpar_def_list; Printf.printf("):"); print_retType header.ret_type Printf.printf(")");
+
 
 let rec print_fparDef fparDef = 
   match fparDef.ref with
   | ""  -> Printf.printf("FparDef("); List.iter print_id fparDef.id_list; Printf.printf(":") print_fparType fparDef.fpar_type; Printf.printf(")");
-  
   | "ref" -> Printf.printf("FparDef(ref"); List.iter print_id fparDef.id_list; Printf.printf(":") print_fparType fparDef.fpar_type; Printf.printf(")");
 
-let rec print_myType myType = 
-  print_dataType myType.data_type; print_arrayDimension myType.array_dimension;
 
-let rec 
+let rec print_dataType dataType =
+  match dataType with
+  | "int" -> Printf.printf("(DataType(int)");
+  | "char" -> Printf.printf("(DataType(char)");
+
+  
+let rec print_myType myType = 
+  Printf.printf("MyType(") ;print_dataType myType.data_type; List.iter (fun x -> Printf.printf "%d" x; Printf.printf(" ")) myType.array_dimension Printf.printf(")");
+
+
+let rec print_retType retType =
+  let help retType =
+    match retType with 
+    | RetDataType dataType -> print_dataType dataType
+    | Nothing nothing -> Printf.printf("nothing");
+  in
+    Printf.printf("(RetType("); help retType; Printf.printf(")");
+
+  
+let rec print_fparType fparType = 
+  Printf.printf("FparType(") ;print_dataType fparType.data_type2; List.iter (fun x -> Printf.printf "%d" x; Printf.printf(" ")) fparType.array_dimension2 Printf.printf(")");
+
+
+let rec print_localDef localDef =
+  let help localDef =
+    match localDef with
+    | L_FuncDef funcDef -> print_funcDef funcDef;
+    | L_FuncDecl funcDecl -> print_funcDecl funcDecl;
+    | L_varDef varDef -> print_varDef varDef;
+  in
+    Printf.printf("LocalDef("); help localDef; Printf.prinf(")");
+
+  
+let rec print_funcDecl funcDecl = 
+  Printf.printf("FuncDecl("); print_header funcDecl; Printf.printf(";)");
+
+let rec print_idList idList =
+  match idList with 
+  | h -> Printf.printf("%s") h;
+  | h :: tail -> if tail = [] then Printf.printf ("%s") h
+                 else Printf.printf("%s ,") h; print_idList tail;
+
+let rec print_varDef varDef =
+  Printf.printf("VarDef(var"); print_idList varDef.id_list; Printf.printf(":"); print_myType varDef.mytype; Printf.printf(";");
+
+
+let rec print_stmt stmt =
+  match stmt with
+  | S_assignment (l, e)   -> Printf.printf("Assignment("); print_lvalue l; Printf.printf("<-") print_expr e; Printf.printf(";)");
+  | S_block stmt_list     -> Printf.printf("Block("); List.iter print_stmt stmt_list; Printf.printf(")");
+  | S_func_call f         -> print_funcCall f; Printf.printf(";");
+  | S_if (c, s)           -> Printf.printf("If(If("); print_cond c; Printf.printf("), Then("); print_stmt s; Printf.printf("))");
+  | S_if_else (c, s1, s2) -> Printf.printf("IfElse("); print_cond c; Printf.printf("), Then("); print_stmt s1; 
+                             Printf.printf("), Else"); print_stmt s2; Printf.printf("))");
+  | S_while (c, s)        -> Printf.printf("While("); print_cond c; print_stmt s; Printf.printf(")");
+  | S_return e            -> Printf.printf("Return("); print_expr e; Printf.printf(";)");
+  | S_semicolon           -> Printf.printf("Semicolon(;)");
+
+
+let rec printf_funcCall funcCall =
+  Printf.printf("FuncCall("); Printf.printf("%s ") funcCall.id; print_exprList funcCall.exrp_list; Printf.printf(")");
+
+
+let rec print_lvalue lvalue =
+  let help lvalue =
+    match lvalue with
+    | L_id str      -> Printf.printf("%s ") str;
+    | L_string str  -> Printf.printf("% s") str;
+    | L_comp (l, e) -> print_lvalue l; Printf.printf("[") print_expr e; Printf.printf("]");
+  in
+    Printf.printf("Lvalue(") help lvalue; Printf.printf(")");
+
+
+let rec print_expr expr =
+  
