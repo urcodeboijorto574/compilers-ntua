0b763c2aa893a58cfa4a618858b3fe1a0b49a624
Dimitris Vassiliou <dhmhtrhs.vassiliou@gmail.com>
2023-10-23 16:46:41 +0300

    Added build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Execute 'make clean'
+make clean
+
+# Check the exit status of 'make clean'
+if [ $? -eq 0 ]; then
+  # 'make clean' was successful, so proceed with 'make depend'
+  make depend
+
+  # Check the exit status of 'make depend'
+  if [ $? -eq 0 ]; then
+    # 'make depend' was successful, so proceed with 'make'
+    make
+  else
+    echo "Error: 'make depend' failed."
+  fi
+else
+  echo "Error: 'make clean' failed."
+fi
