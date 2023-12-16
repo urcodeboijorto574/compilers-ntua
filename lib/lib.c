508b46f58a036099519ed1d3ca786831e0a6784e
Ioannis Giannoukos <ioannhsyan76@gmail.com>
2023-12-16 14:16:13 +0200

    Implementations of string std-lib funcs deleted
    Their implementation can be directly associated with the according ones
    from string.h C library.

--- lib/lib.c
+++ lib/lib.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <string.h>
 
 extern void writeInteger(int x) {
     printf("%d", x);
@@ -42,56 +43,4 @@ extern int ascii (char c) {
 
 extern char chr (int n) {
     return (char)n;
-}
-
-extern int strlen (const char *s) {
-    if (!s) {
-        fprintf(stderr, "Error: Null pointer detected in strlen function.\n");
-        return -1;
-    }
-
-    int i = -1, counter = 0;
-    while (s[++i] != '\0')
-        ++counter;
-    return counter;
-}
-
-extern int strcmp (const char *s1, const char *s2) {
-    if (!s1 || !s2) {
-        fprintf(stderr, "Error: Null pointer detected in strcmp function.\n");
-        return !s1 ? -2 : 2;
-    }
-
-    int i = 0;
-    while (s1[i] == s2[i])
-        if (s1[i++] == '\0')
-            return 0;
-
-    return (int)s1[i] - (int)s2[i] > 0 ? 1 : -1;
-}
-
-extern void strcpy (char * trg, const char *src) {
-    if (!trg || !src) {
-        fprintf(stderr, "Error: Null pointer detected in strcpy function.\n");
-        return;
-    }
-
-    int i = 0;
-    do trg[i] = src[i];
-    while (src[i++] != '\0');
-    return;
-}
-
-extern void strcat (char * trg, const char *src) {
-    if (!trg || !src) {
-        fprintf(stderr, "Error: Null pointer detected in strcat function.\n");
-        return;
-    }
-
-    int i = -1;
-    while (trg[++i] != '\0') ;
-    int j = 0;
-    do trg[i] = src[j];
-    while (src[j++] != '\0') ;
-    return;
 }
