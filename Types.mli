9f972f89c4dcfd510a48af028c8246f0389a0dac
Ioannis Giannoukos <ioannhsyan76@gmail.com>
2023-12-27 16:27:49 +0200

    Added doc string for t_type_of_t_func function

--- Types.mli
+++ Types.mli
@@ -24,6 +24,8 @@ val construct_array_type : int list -> t_type -> t_type
     the same. If they are the same [true] is returned, otherwise [false]. *)
 val equal_types : t_type -> t_type -> bool
 
+(** [t_type_of_t_func t] takes a type [T_func ti] type and returns the
+    encapsulated type [ti]. Returns [Types.t_type] *)
 val t_type_of_t_func : t_type -> t_type
 
 (* Functions that convert types defined in Ast to t_type types are defined in
