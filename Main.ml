c5cc9ff0618c12d99af817b3f9bf1518d12ba6e0
Ioannis Giannoukos <ioannhsyan76@gmail.com>
2023-12-28 22:35:08 +0200

    Flushing output in Main.ml after syntax & semantic analysis and code gen

--- Main.ml
+++ Main.ml
@@ -9,13 +9,13 @@ let main =
       Printf.printf "Syntactic analysis:\n";
       PrintAst.print_on asts;
       Printf.printf "\n");
-    Printf.printf "\027[32mSuccessful parsing.\027[0m\n";
+    Printf.printf "\027[32mSuccessful parsing.\027[0m\n%!";
     if Types.debugMode then (
       Printf.printf "\n";
       Printf.printf "Semantic analysis:\n");
     SemAst.sem_on asts;
     if Types.debugMode then Printf.printf "\n";
-    Printf.printf "\027[32mSemantically correct.\027[0m\n";
+    Printf.printf "\027[32mSemantically correct.\027[0m\n%!";
     GenAst.gen_on asts;
 
     (* command to compile GenAst.ml
@@ -31,7 +31,7 @@ let main =
        ./a.out
     *)
     print_module "a.ll" GenAst.the_module;
-    Printf.printf "\027[32mIR code generation completed.\027[0m\n"
+    Printf.printf "\027[32mIR code generation completed.\027[0m\n%!"
   with Parsing.Parse_error ->
     Printf.eprintf "Syntax error\n";
     exit 1
