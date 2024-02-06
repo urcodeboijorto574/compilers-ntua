544540d61c4ddbe125163d4593c4389b4e4da80b
Ioannis Giannoukos <ioannhsyan76@gmail.com>
2024-02-06 19:25:26 +0200

    test_programs script can take multiple directories

--- test_programs.sh
+++ test_programs.sh
@@ -5,40 +5,39 @@ totalCounter=0
 errorCounter=0
 correctCounter=0
 
-# Specify the directory
-directory=$1
-
-# Iterate through each file in the directory
-for file in "$directory"*.grc
+# Iterate through each file in the directories given
+for directory in "$@"
 do
-  if [ -f "$file" ]; then
-    ((++totalCounter))
-    # Extracting the file name without the directory path
-    filename=$(basename "$file")
+  for file in "$directory"*.grc
+  do
+    if [ -f "$file" ]; then
+      ((++totalCounter))
+      # Extracting the file name without the directory path
+      filename=$(basename "$file")
 
-    # Remove any previously created executable
-    if [ -f "./a.out" ]; then
-      rm ./a.out
+      # Remove any previously created executable
+      if [ -f "./a.out" ]; then
+        rm ./a.out
+      fi
+
+      # Print a start message
+      echo "$filename:"
+
+      # Run the file
+      ./grace "$file"
+
+      # If compilation was successful, run the executable
+      if [ -f "./a.out" ]; then
+        ((++correctCounter))
+        ./a.out
+      else
+        ((++errorCounter))
+      fi
+
+      # Add a newline for better output separation
+      echo
     fi
-
-    # Print a start message
-    echo "$filename:"
-
-    # Run the file
-    ./grace "$file"
-
-    # If compilation was successful, run the executable
-    if [ -f "./a.out" ]; then
-      ((++correctCounter))
-      ./a.out
-    else
-      ((++errorCounter))
-    fi
-
-    # Add a newline for better output separation
-    echo
-  fi
+  done
 done
-
 echo "Correct programs: $correctCounter/$totalCounter"
 echo "Erroneous programs: $errorCounter/$totalCounter"
